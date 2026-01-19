6ca13b69321bbaf4341800bf296517262801b31d
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2026-01-19 15:39:51 +0200

    fixing chat bugs

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "./src/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|.*\\..*|[a-zA-Z-]+/api).*)"],
 };
